65bd9f70d03ab2644a2eb5e934a38ba1d342408e
Юрчик <88848940+Yrchik07@users.noreply.github.com>
2024-01-22 20:44:55 +0200

    'update'

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,22 +1,39 @@
 import "../css/form.css"
 
 const form = document.querySelector(".feedback-form");
+const emailInput = form.querySelector('input');
+const messageInput = form.querySelector('textarea');
 
-form.addEventListener("submit", handleFormSubmit);
+emailInput.addEventListener('input', saveFormState);
+messageInput.addEventListener('input', saveFormState);
+form.addEventListener('submit', handleSubmit);
 
-function handleFormSubmit(event) {
-  event.preventDefault();
-  const form = event.target;
-  const email = form.elements.email.value.trim();
-  const password = form.elements.message.value.trim();
-
-  if (email === "" || message === "") {
-     alert("All form fields must be filled in");
-  }else{
-    let formData = {
-      email: email,
-      message: message
+function saveFormState() {
+  const formState = {
+    email: emailInput.value.trim(),
+    message: messageInput.value.trim()
   };
-  console.log(formData);
-  form.reset();
-}}
+  localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+}
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  const savedFormState = localStorage.getItem('feedback-form-state');
+  if (savedFormState) {
+    const formState = JSON.parse(savedFormState);
+    emailInput.value = formState.email;
+    messageInput.value = formState.message;
+  }
+});
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const formState = {
+    email: emailInput.value.trim(),
+    message: messageInput.value.trim()
+  };
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+  console.log(formState);
+}
